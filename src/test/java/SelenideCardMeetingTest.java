91442348405436c0cffa90b63072368c820e75f0
anna_stjarna <artemyeva@gmail.com>
2023-04-25 15:58:41 +0300

    Test code added

--- src/test/java/SelenideCardMeetingTest.java
+++ src/test/java/SelenideCardMeetingTest.java
@@ -1,2 +1,30 @@
+import com.codeborne.selenide.Condition;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
+
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
 public class SelenideCardMeetingTest {
+
+    public String localDate(int day) {
+        return LocalDate.now().plusDays(day).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+    }
+
+    @Test
+    void shouldDeliverCard() {
+        open("http://localhost:9999/");
+        $("[data-test-id='city' ] input").setValue("Нижний Новгород");
+        $("[data-test-id='date'] input").sendKeys(Keys.SHIFT, Keys.HOME, Keys.BACK_SPACE);
+        $("[data-test-id='date'] input").setValue(localDate(3));
+        $("[data-test-id='name'] input").setValue("Иванова Алевтина");
+        $("[data-test-id='phone'] input").setValue("+79999999999");
+        $("[data-test-id='agreement']").click();
+        $(".button__content").click();
+        $(".notification__content").shouldHave(Condition.text("Встреча успешно забронирована на " + localDate(3)), Duration.ofSeconds(15)).shouldBe(Condition.visible);
+    }
 }
